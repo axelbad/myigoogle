f40317c2b3dfeec647dc14764dad52e28368343f
Alessandro Badiglio <a.badiglio@pindarcreative.co.uk>
2021-04-26 14:19:57 +0100

    Merged with inettuts3

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,8 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Feed;
-use Illuminate\Http\Request;
 use App\Models\Feed_tab;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
@@ -36,7 +37,8 @@ class HomeController extends Controller
             $current_tab_id = $feeds_tabs->first()->id;
         }
 
-        $feeds = Feed::all()->where('tab_feed_id', $current_tab_id);
+        //$feeds = Feed::all()->where('tab_feed_id', $current_tab_id)->orderBy('col', 'ASC');
+        $feeds = DB::table('feeds')->where('tab_feed_id', $current_tab_id)->orderBy('col', 'ASC')->get();
 
         return view('home')->with(array('feeds_tabs' => $feeds_tabs, 'feeds' => $feeds));
     }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,11 +4,20 @@
     <div class="container-fluid">
 
         <div id="columns">
-
-            <ul id="column1" class="column">
-                <li class="widget color-green" id="intro">
+<?php
+        $num_colonne = $feeds->max('col');
+        for($i=1; $i<=$num_colonne; $i++)
+		{
+            $filtered = $feeds->where('col', $i);
+?>
+            <ul id="column{{ $i }} ?>" class="column">
+<?php
+            $filtered->each(function ($item, $key)
+            {
+?>
+                <li class="widget {{ $item->color }}" id="intro">
                     <div class="widget-head">
-                        <h3>Introduction Widget</h3>
+                        <h3>{{ $item->title }}</h3>
                     </div>
                     <div class="widget-content">
                         <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aliquam magna sem, fringilla in,
@@ -17,70 +26,13 @@
                             lectus.</p>
                     </div>
                 </li>
-                <li class="widget color-red">
-                    <div class="widget-head">
-                        <h3>Widget title</h3>
-                    </div>
-                    <div class="widget-content">
-                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aliquam magna sem, fringilla in,
-                            commodo a, rutrum ut, massa. Donec id nibh eu dui auctor tempor. Morbi laoreet eleifend dolor.
-                            Suspendisse pede odio, accumsan vitae, auctor non, suscipit at, ipsum. Cras varius sapien vel
-                            lectus.</p>
-                    </div>
-                </li>
+<?php
+            });
+?>
             </ul>
-
-            <ul id="column2" class="column">
-                <li class="widget color-blue">
-                    <div class="widget-head">
-                        <h3>Widget title</h3>
-                    </div>
-                    <div class="widget-content">
-                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aliquam magna sem, fringilla in,
-                            commodo a, rutrum ut, massa. Donec id nibh eu dui auctor tempor. Morbi laoreet eleifend dolor.
-                            Suspendisse pede odio, accumsan vitae, auctor non, suscipit at, ipsum. Cras varius sapien vel
-                            lectus.</p>
-                    </div>
-                </li>
-                <li class="widget color-yellow">
-                    <div class="widget-head">
-                        <h3>Widget title</h3>
-                    </div>
-                    <div class="widget-content">
-                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aliquam magna sem, fringilla in,
-                            commodo a, rutrum ut, massa. Donec id nibh eu dui auctor tempor. Morbi laoreet eleifend dolor.
-                            Suspendisse pede odio, accumsan vitae, auctor non, suscipit at, ipsum. Cras varius sapien vel
-                            lectus.</p>
-                    </div>
-                </li>
-            </ul>
-
-            <ul id="column3" class="column">
-                <li class="widget color-orange">
-                    <div class="widget-head">
-                        <h3>Widget title</h3>
-                    </div>
-                    <div class="widget-content">
-                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aliquam magna sem, fringilla in,
-                            commodo a, rutrum ut, massa. Donec id nibh eu dui auctor tempor. Morbi laoreet eleifend dolor.
-                            Suspendisse pede odio, accumsan vitae, auctor non, suscipit at, ipsum. Cras varius sapien vel
-                            lectus.</p>
-                    </div>
-                </li>
-                <li class="widget color-white">
-                    <div class="widget-head">
-                        <h3>Widget title</h3>
-                    </div>
-                    <div class="widget-content">
-                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aliquam magna sem, fringilla in,
-                            commodo a, rutrum ut, massa. Donec id nibh eu dui auctor tempor. Morbi laoreet eleifend dolor.
-                            Suspendisse pede odio, accumsan vitae, auctor non, suscipit at, ipsum. Cras varius sapien vel
-                            lectus.</p>
-                    </div>
-                </li>
-
-            </ul>
-
+<?php
+        }
+?>
         </div>
 
     </div>
